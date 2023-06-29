cb5f657f32e8b93765b3776445fb398e26304a23
Jocelyn <jdupuis@my365.bellevue.edu>
2023-06-29 15:21:57 -0400

    update

---
+++ models/customer.js
@@ -0,0 +1,9 @@
+/*
+============================================
+; Title: customer.js
+; Author: Jocelyn Dupuis
+; Date: 06/21/2023
+; Description: Customer file for Pets-R-Us App
+============================================
+*/
+
