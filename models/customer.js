830976da498bb9fa3583ba7e285ccdc340b491a4
Jocelyn <jdupuis@my365.bellevue.edu>
2023-06-30 14:17:12 -0400

    commit

--- models/customer.js
+++ models/customer.js
@@ -7,3 +7,16 @@
 ============================================
 */
 
+// Requires mongoose
+const mongoose = require('mongoose');
+
+// Customer schema with customer fields: id and email
+let customerSchema = new mongoose.Schema({
+    customerId: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true }
+});
+
+// Exports the customer module
+module.exports = mongoose.model('Customer', customerSchema);
+
+
